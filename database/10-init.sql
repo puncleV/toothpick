9ffb388c4e189a90d173bfa4145432a1eac48144
Viktor Kozlov <punkrus18@gmail.com>
2019-07-06 14:54:03 +0300

    use camel case for columns names

--- database/10-init.sql
+++ database/10-init.sql
@@ -11,13 +11,13 @@ CREATE TABLE IF NOT EXISTS pizza_size (
 CREATE TABLE IF NOT EXISTS pizza (
    id UUID PRIMARY KEY,
    name VARCHAR(25) NOT NULL,
-   pizza_size UUID NOT NULL,
-   pizza_base UUID NOT NULL,
+   pizzaSize UUID NOT NULL,
+   pizzaBase UUID NOT NULL,
    description VARCHAR(255),
-   CONSTRAINT pizza_pizza_size FOREIGN KEY (pizza_size)
+   CONSTRAINT pizza_pizza_size FOREIGN KEY (pizzaSize)
     REFERENCES pizza_size (id)
     ON UPDATE CASCADE ON DELETE RESTRICT,
-   CONSTRAINT pizza_pizza_base FOREIGN KEY (pizza_base)
+   CONSTRAINT pizza_pizza_base FOREIGN KEY (pizzaBase)
     REFERENCES pizza_base (id)
     ON UPDATE CASCADE ON DELETE RESTRICT
 );
@@ -33,21 +33,21 @@ CREATE TABLE IF NOT EXISTS customer_order (
    status VARCHAR(25) NOT NULL,
    created VARCHAR(25) NOT NULL,
    delivered UUID NOT NULL,
-   customer_id UUID NOT NULL,
-   CONSTRAINT customer_order_customer_id FOREIGN KEY (customer_id)
+   customerId UUID NOT NULL,
+   CONSTRAINT customer_order_customer_id FOREIGN KEY (customerId)
     REFERENCES customer (id)
     ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS customer_order_pizza (
    id UUID PRIMARY KEY,
-   customer_order_id UUID NOT NULL,
-   pizza_id UUID NOT NULL,
+   customerOrderId UUID NOT NULL,
+   pizzaId UUID NOT NULL,
    count smallint DEFAULT 1,
-  CONSTRAINT customer_order_pizza_customer_order FOREIGN KEY (customer_order_id)
+  CONSTRAINT customer_order_pizza_customer_order FOREIGN KEY (customerOrderId)
     REFERENCES customer_order (id)
     ON UPDATE CASCADE ON DELETE RESTRICT,
-  CONSTRAINT customer_order_pizza_pizza FOREIGN KEY (pizza_id)
+  CONSTRAINT customer_order_pizza_pizza FOREIGN KEY (pizzaId)
     REFERENCES pizza (id)
     ON UPDATE CASCADE ON DELETE RESTRICT
 );
